f8d2e0c084a6cb8ff69666dc5b2cb0e3f79c9424
Ferdia <ferdia.o.corragain@rabobank.nl>
2022-09-27 11:52:14 +0200

    Initial commit

---
+++ src/test/java/net/ocorragain/dockeraws/DockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ocorragain.dockeraws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
